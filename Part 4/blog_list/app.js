c7b3bd67d008e23c50466b292709314404d304ae
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-05-09 11:52:25 -0700

    added middleware for user extraction 4.22

--- Part 4/blog_list/app.js
+++ Part 4/blog_list/app.js
@@ -23,12 +23,10 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
-app.use(middleware.tokenExtractor)
-
 module.exports = app
