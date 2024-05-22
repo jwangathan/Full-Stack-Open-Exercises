40820c28aecba72ab028d3aca93c84d891b8fe8f
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-05-21 20:22:40 -0700

    5.2 implemented logout/permanent local storage

--- Part 5/bloglist-frontend/src/App.jsx
+++ Part 5/bloglist-frontend/src/App.jsx
@@ -25,6 +25,7 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -34,6 +35,14 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    console.log('logging out user', user.name)
+
+    window.localStorage.clear()
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -70,7 +79,10 @@ const App = () => {
         </div> :
         <div>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+          <p>
+            {user.name} logged in
+            <button onClick={handleLogout}>logout</button>
+          </p>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
