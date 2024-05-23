c71c7bb9a4552136a2b27194f77cbe52992fa156
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-05-22 22:39:07 -0700

    remove console log statement

--- Part 4/blog_list/utils/middleware.js
+++ Part 4/blog_list/utils/middleware.js
@@ -40,7 +40,6 @@ const tokenExtractor = (request, response, next) => {
 }
 
 const userExtractor = async (request, response, next) => {
-  console.log(request.token)
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({
