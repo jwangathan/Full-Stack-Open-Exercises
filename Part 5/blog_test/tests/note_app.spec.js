d5114c7fd07dccf7038caea383ff8bdac4d43543
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-06-11 12:43:54 -0700

    5.19 added test for blog creation

--- Part 5/blog_test/tests/note_app.spec.js
+++ Part 5/blog_test/tests/note_app.spec.js
@@ -1,8 +1,8 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith, createNote } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
-    await page.pause()
     await request.post('/api/testing/reset')
     await request.post('/api/users', {
       data: {
@@ -22,23 +22,30 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-        await page.getByTestId('username').fill('jonathan_wang')
-        await page.getByTestId('password').fill('password')
-        await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'jonathan_wang', 'password')
 
-        await expect(page.getByText('Jonathan Wang logged in')).toBeVisible()
+      await expect(page.getByText('Jonathan Wang logged in')).toBeVisible()
     })
   
     test('fails with wrong credentials', async ({ page }) => {
-      await page.getByTestId('username').fill('jonathan_wang')
-      await page.getByTestId('password').fill('wrong')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'jonathan_wang', 'wrong')
 
       const errorDiv = await page.locator('.error')
       await expect(errorDiv).toContainText('wrong credentials')
       await expect(errorDiv).toHaveCSS('border-style', 'solid')
       await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
       await expect(page.getByText('Jonathan Wang logged in')).not.toBeVisible()
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'jonathan_wang', 'password')
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await createNote(page, 'a title', 'an author', 'url.com')
+      await expect(page.getByText('a title - an author')).toBeVisible()
     })
   })
 })
