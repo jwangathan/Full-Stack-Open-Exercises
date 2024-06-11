ca0b5cf57137b5702a901ec6f3709b3812808f04
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-06-11 01:47:47 -0700

    5.18 added testing for successfuul/failure of login

--- Part 5/blog_test/tests/note_app.spec.js
+++ Part 5/blog_test/tests/note_app.spec.js
@@ -1,11 +1,44 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
-        await page.goto('/')
+  beforeEach(async ({ page, request }) => {
+    await page.pause()
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Jonathan Wang',
+        username: 'jonathan_wang',
+        password: 'password'
+      }
     })
-    test('Login form is shown', async ({ page }) => {
-        const locator = await page.getByText('Log in to application')
-        await expect(locator).toBeVisible()
+    
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const locator = await page.getByText('Log in to application')
+    await expect(locator).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+        await page.getByTestId('username').fill('jonathan_wang')
+        await page.getByTestId('password').fill('password')
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('Jonathan Wang logged in')).toBeVisible()
     })
+  
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('jonathan_wang')
+      await page.getByTestId('password').fill('wrong')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('wrong credentials')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+      await expect(page.getByText('Jonathan Wang logged in')).not.toBeVisible()
+    })
+  })
 })
