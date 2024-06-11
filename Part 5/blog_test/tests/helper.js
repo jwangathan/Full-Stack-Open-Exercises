0ed7fa10fd5f726d54c403d145ba0975f23e72d1
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-06-11 13:03:38 -0700

    5.20 implemented testing for like functionality

--- Part 5/blog_test/tests/helper.js
+++ Part 5/blog_test/tests/helper.js
@@ -5,7 +5,6 @@ const loginWith = async (page, username, password)  => {
   }
   
   const createNote = async (page, title, author, url) => {
-    await page.pause()
     await page.getByRole('button', { name: 'new blog' }).click()
     await page.getByTestId('title').fill(title)
     await page.getByTestId('author').fill(author)
--- Part 5/blog_test/tests/note_app.spec.js
+++ Part 5/blog_test/tests/note_app.spec.js
@@ -47,5 +47,22 @@ describe('Blog app', () => {
       await createNote(page, 'a title', 'an author', 'url.com')
       await expect(page.getByText('a title - an author')).toBeVisible()
     })
+
+    describe('and a blog exists', () => {
+      beforeEach(async ({ page }) => {
+        await createNote(page, 'title 1', 'author 1', 'url1.com')
+        await createNote(page, 'title 2', 'author 2', 'url2.com')
+      })
+
+      test('a blog can be liked', async ({ page }) => {
+        const blogText = await page.getByText('title 2 - author 2')
+        const blogElement = await blogText.locator('..')
+
+        await blogElement.getByRole('button', { name: 'view' }).click()
+
+        await page.getByRole('button', { name: 'like' }).click()
+        await expect(blogElement.locator('.viewContent').getByText('likes: 1')).toBeVisible()
+      })
+    })
   })
 })
