54374ee12cc77734f9b91a9c52cf7256710b735d
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-06-13 11:58:39 -0700

    6.4 implemented functionality for adding anecdotes

--- Part 6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part 6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -24,6 +24,8 @@ const reducer = (state = initialState, action) => {
   console.log('action', action)
 
   switch(action.type) {
+    case 'NEW_ANECDOTE':
+      return [...state, action.payload]
     case 'VOTE':
       const id = action.payload.id
       const ancedoteToChange = state.find(a => a.id === id)
@@ -44,6 +46,17 @@ export const vote = (id) => {
   return {
     type: 'VOTE',
     payload: { id }
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: {
+      content,
+      id: getId(),
+      votes: 0
+    }
   }
 }
 
