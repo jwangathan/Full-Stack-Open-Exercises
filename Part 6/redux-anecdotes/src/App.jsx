c36601d30b913c0b8dd7cc013c197b6c91a3b04c
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-06-13 16:03:21 -0700

    6.6-6.8 separated the creation of action-objects into action creator functions and separated the form and list into its own modules

--- Part 6/redux-anecdotes/src/App.jsx
+++ Part 6/redux-anecdotes/src/App.jsx
@@ -1,36 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { vote, createAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecoteList'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-  
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.sort((a1, a2) => a2.votes - a1.votes).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name='anecdote'/></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
---
+++ Part 6/redux-anecdotes/src/components/AnecoteList.jsx
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import { vote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleClick }) => {
+	return (
+		<div>
+			<div>
+				{anecdote.content}
+			</div>
+			<div>
+				has {anecdote.votes}
+				<button onClick={handleClick}>vote</button>
+			</div>
+		</div>
+	)
+}
+
+const AnecdoteList = () => {
+    const dispatch = useDispatch()
+    const anecdotes = useSelector(state => state)
+
+    return (
+			<div>
+				{anecdotes.sort((a1, a2) => a2.votes - a1.votes).map(anecdote =>
+					<Anecdote
+						key={anecdote.id}
+						anecdote={anecdote}
+						handleClick={() => dispatch(vote(anecdote.id))}
+					/>
+				)}
+			</div>
+    )
+}
+
+export default AnecdoteList
