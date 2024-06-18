6d15a07ff74ff7cbffd4d3c51aa8104e2144659e
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-06-18 00:39:03 -0700

    6.15 creation of new anecdotes stores it in the backend

--- Part 6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ Part 6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,17 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification, setEmpty } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newNote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newNote))
     dispatch(setNotification(`you created the anecdote '${content}'`))
     setTimeout(() => {
       dispatch(setEmpty())
