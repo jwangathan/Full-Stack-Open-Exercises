3b9ebe92afdf795c962ebfb52b33bb474b7a91f6
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-06-18 02:05:35 -0700

    6.16 implemented initialization with Redux Thunk

--- Part 6/redux-anecdotes/src/App.jsx
+++ Part 6/redux-anecdotes/src/App.jsx
@@ -3,17 +3,15 @@ import AnecdoteForm from './components/AnecdoteForm'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
 import { useEffect } from 'react'
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    anecdoteService
-      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
-  }, [dispatch])
-  
+    dispatch(initializeAnecdotes())
+  }, [])
+
   return (
     <div>
       <Notification />
