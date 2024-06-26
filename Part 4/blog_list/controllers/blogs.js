7526b8010040e480a46293ab2c0923afcd3a56e7
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-06-26 15:04:09 -0700

    7.18/7.19 added functionality to add comments to blogs

--- Part 4/blog_list/controllers/blogs.js
+++ Part 4/blog_list/controllers/blogs.js
@@ -3,71 +3,97 @@ const Blog = require('../models/blog')
 const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({}).populate('user', { username: 1, name: 1 })
-  response.json(blogs)
+	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+	response.json(blogs)
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body
+	const body = request.body
 
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  }
+	const blog = {
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: body.likes,
+	}
 
-  const updateBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  await updateBlog.populate('user', { username: 1, name: 1})
-  response.json(updateBlog)
+	const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+		new: true,
+	})
+	await updatedBlog.populate('user', { username: 1, name: 1 })
+	response.json(updatedBlog)
 })
 
 blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
-  const body = request.body
-  const user = request.user
+	const body = request.body
+	const user = request.user
 
-  if (!body.title || !body.author) {
-    return response.status(400).end()
-  }
+	if (!body.title || !body.author) {
+		return response.status(400).end()
+	}
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
-    user: user._id
-  })
+	const blog = new Blog({
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: body.likes || 0,
+		user: user._id,
+	})
 
-  await blog.populate('user', { username: 1, name: 1 })
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+	await blog.populate('user', { username: 1, name: 1 })
+	const savedBlog = await blog.save()
+	user.blogs = user.blogs.concat(savedBlog._id)
+	await user.save()
 
-  response.status(201).json(savedBlog)
+	response.status(201).json(savedBlog)
+})
+
+blogsRouter.post('/:id/comments', async (request, response) => {
+	const body = request.body
+	if (!body.content) {
+		return response.status(400).json({ error: 'Comment content is missing' })
+	}
+	const blog = await Blog.findById(request.params.id)
+
+	if (!blog) {
+		return response.status(400).json({ error: 'Blog not found' })
+	}
+
+	blog.comments.push({ content: body.content })
+	await blog.populate('user', { username: 1, name: 1 })
+
+	const savedBlog = await blog.save()
+
+	response.status(201).json(savedBlog)
 })
 
 blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
-  if (blog) {
-    response.json(blog)
-  } else {
-    response.status(404).end()
-  }
+	const blog = await Blog.findById(request.params.id)
+	if (blog) {
+		response.json(blog)
+	} else {
+		response.status(404).end()
+	}
 })
 
-blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
-  const user = request.user
+blogsRouter.delete(
+	'/:id',
+	middleware.userExtractor,
+	async (request, response) => {
+		const user = request.user
 
-  const blog = await Blog.findById(request.params.id)
+		const blog = await Blog.findById(request.params.id)
 
-  if ( blog.user.toString() !== user.id.toString() ) {
-    return response.status(401).json({ error: 'only author of this blog can delete' })
-  }
+		if (blog.user.toString() !== user.id.toString()) {
+			return response
+				.status(401)
+				.json({ error: 'only author of this blog can delete' })
+		}
 
-  await Blog.findByIdAndDelete(request.params.id)
+		await Blog.findByIdAndDelete(request.params.id)
 
-  response.status(204).end()
-})
+		response.status(204).end()
+	}
+)
 
 module.exports = blogsRouter
