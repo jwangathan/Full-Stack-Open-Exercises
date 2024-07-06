8f2ebd53c6285b2b7d93b88c2962cf167f27c69e
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-07-05 19:04:42 -0700

    8.5 include genre filtering:

--- Part 8/library-backend/index.js
+++ Part 8/library-backend/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
 		authorCount: Int!
-		allBooks(author: String): [Book!]!
+		allBooks(author: String, genre: String): [Book!]!
 		allAuthors: [Author!]!
   }
 `
@@ -126,10 +126,12 @@ const resolvers = {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
 		allBooks: (root, args) => {
-			if (!args.author) {
-				return books
-			}
-			return books.filter((b) => b.author === args.author)
+			return books.filter((b) => {
+				return (
+					(!args.author || b.author === args.author) &&
+					(!args.genre || b.genres.includes(args.genre))
+				)
+			})
 		},
 		allAuthors: () => authors,
 	},
