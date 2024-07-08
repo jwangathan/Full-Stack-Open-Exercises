0a7c2a25897d55f07489a4ed5a024ad25a27df18
Jonathan Wang <jonathan_wang@ucsb.edu>
2024-07-08 00:30:48 -0700

    8.17 fixed listing books

--- Part 8/library-frontend/src/queries.jsx
+++ Part 8/library-frontend/src/queries.jsx
@@ -15,7 +15,9 @@ export const ALL_BOOKS = gql`
 	query {
 		allBooks {
 			title
-			author
+			author {
+				name
+			}
 			published
 			id
 		}
@@ -54,3 +56,11 @@ export const EDIT_AUTHOR = gql`
 		}
 	}
 `
+
+// export const LOGIN = gql`
+// 	mutation login($username: String!, password: String!) {
+// 		login(username: $username, password: $password) {
+// 			value
+// 		}
+// 	}
+// `
